83c90218c4e31ba2e81717b33343eabae3300921
jishan1019 <imransilamjishan80@gmail.com>
2024-12-06 17:48:03 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis totam
+      corporis itaque quas. Modi, soluta tempore consequatur sint harum vel
+      tempora quaerat amet eaque, quia minima perspiciatis voluptas. Fugiat,
+      exercitationem.
+    </div>
+  );
+};
+
+export default App;
